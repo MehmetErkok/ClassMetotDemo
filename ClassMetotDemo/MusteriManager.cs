a23d9890cd08141772bfac3893396937dbecb654
Mehmet ERKÖK <mehmet.erkok@akkanatholding.com>
2021-01-23 22:23:48 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> Musteriler = new List<Musteri> { };
+        
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("---Müşteri Ekleme---");
+            Console.WriteLine("{0} Eklenmiştir!!\n", musteri.MusteriAdi);
+            Musteriler.Add(musteri);
+
+        }
+        public void MusteriListesi()
+        {
+            Console.WriteLine("Musteri Listesi");
+            foreach (var Musteri in Musteriler)
+            {
+                Console.WriteLine(
+                    "Adı :"+Musteri.MusteriAdi + " " +
+                    "Soyadı :" +" "+Musteri.MusteriSoyadi + " "+
+                    "Telefon :"  + Musteri.MusteriTelefon  + " "+
+                    "Şube :"  +  Musteri.MusteriSube);
+
+            }
+         }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("---Müşteri Silme---");
+            Console.WriteLine(musteri.MusteriAdi +  " isimli Müşteri Silinmiştir." );
+            
+
+            Musteriler.Remove(musteri);
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bankamıza Hoşgeldiniz");
+            Musteri musteri = new Musteri();
+            musteri.MusteriId =1;
+            musteri.MusteriAdi = "Mehmet";
+            musteri.MusteriSoyadi = "Büşra";
+            musteri.MusteriTelefon = 11111111;
+            musteri.MusteriSube = "İstanbul";
+
+
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 2;
+            musteri1.MusteriAdi = "Büşra";
+            musteri1.MusteriSoyadi = "Mehmet";
+            musteri1.MusteriTelefon = 222222;
+            musteri1.MusteriSube = "İstanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 3;
+            musteri2.MusteriAdi = "Şerife";
+            musteri2.MusteriSoyadi="Oğuzhan";
+            musteri2.MusteriTelefon=333333;
+            musteri2.MusteriSube = "İstanbul";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 4;
+            musteri3.MusteriAdi = "Oğuzhan";
+            musteri3.MusteriSoyadi = "Şerife";
+            musteri3.MusteriTelefon = 44444;
+            musteri3.MusteriSube="İstanbul";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme(musteri);
+            musteriManager.MusteriEkleme(musteri1);
+            musteriManager.MusteriEkleme(musteri2);
+            musteriManager.MusteriEkleme(musteri3);
+
+            musteriManager.MusteriListesi();
+
+
+            musteriManager.MusteriSilme(musteri3);
+            musteriManager.MusteriSilme(musteri);
+
+
+            musteriManager.MusteriListesi();
+
+
+
+
+
+
+
+
+        }
+    }
+}
